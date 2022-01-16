eb6b29b9cf8c71bad227aa8e15684825c51c5ec0
Eunki <thddmsrlthd@gmail.com>
2022-01-17 01:33:44 +0900

    나머지 파일들

---
+++ Silver_1/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+    // for(NODE* p = head -> next; p != NULL; p = p -> next) // p = head가 아닌 head -> next인 이유는 head 부터 시작하면 head에 포함된 NULL값이 그대로 출력되기 떄문
+    //     printf("%d -> ", p -> data);
+    
+    // printf("NULL\n");
